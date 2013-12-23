1bfd6bb634c64ce9139678f22321b58a0f08cf1c
itair <itair@163.com>
2013-12-23 15:14:27 +0800

    C++ Primer Plus 课后习题 p34 2.7

---
+++ try.cpp
@@ -0,0 +1,20 @@
+//C++ Primer Plus 课后习题 p33 2.7 
+// by itAir
+// Try to use Google C++ programing style
+
+// dir2/foo2.h（优先位置，详情如下）
+//C 系统文件
+//	C++系统文件
+//	其他库头文件
+//	本项目内头文件
+
+#include "try.h"
+#include <iostream>
+
+using namespace std;\
+
+void main(void){
+	cout<<"Hello World!"<<endl;
+	system("pause");
+// return 1;
+}
